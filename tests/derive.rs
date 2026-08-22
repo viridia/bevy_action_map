5a7a69e7233a53b7332a7b8e8744ff75498ddf58
Talin <viridia@gmail.com>
2026-08-22 14:00:18 -0700

    Fix incorrect proc macro test.

--- tests/derive.rs
+++ tests/derive.rs
@@ -5,6 +5,7 @@ fn derives_compile() {
     let test_cases = trybuild::TestCases::new();
     test_cases.pass("tests/ui/pass/action.rs");
     test_cases.pass("tests/ui/pass/context.rs");
+    test_cases.compile_fail("tests/ui/fail/missing_attrs.rs");
     test_cases.compile_fail("tests/ui/fail/missing_action_path.rs");
     test_cases.compile_fail("tests/ui/fail/missing_context_path.rs");
 }
--- tests/ui/fail/missing_attrs.rs
+++ tests/ui/fail/missing_attrs.rs
@@ -1,5 +1,4 @@
 #[derive(bevy_action_map::InputAction)]
-#[action(output = bool, intent = Button)]
 struct MissingArgs;
 
 fn main() {}
